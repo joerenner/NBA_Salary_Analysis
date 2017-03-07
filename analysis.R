20079c99fe12ff9935782384d203bf00e390b6df
joerenner <rennerj2@students.wwu.edu>
2017-03-07 15:59:46 +0100

    started EDA

--- analysis.R
+++ analysis.R
@@ -1,1 +1,17 @@
-test
+#Load salary data and drop unnecessary columns 
+salaries <- read.table("salaries.txt", header = TRUE,sep = ",",quote="", stringsAsFactors=FALSE)
+keep <- c("Rk", "Player", "Tm", "X2016.17")
+salaries <- salaries[keep]
+
+#convert from chars to ints
+salaries$X2016.17 <- as.numeric(sub('\\$','',as.character(salaries$X2016.17)))
+salaries$Rk <- as.numeric(sub('\\$','',as.character(salaries$Rk)))
+
+library(ggplot2)
+ggplot(data.frame(salaries$X2016.17), aes(salaries$X2016.17)) + stat_bin(bins=25)
+
+#looks like log-normal distribution
+sal.log <- log10(salaries$X2016.17)
+sallog <- ggplot(data.frame(sal.log), aes(sal.log)) + geom_freqpoly(bins=30)
+sallog
+
